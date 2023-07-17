666a00b4e18adc009b7cca088dd5d724902e39c4
Lex Nastin <lexnastin@gmail.com>
2023-07-18 01:25:27 +0300

    add initial stuff

---
+++ .zshcustomload.sh
@@ -0,0 +1,41 @@
+setopt correct
+setopt extendedglob
+setopt nocaseglob
+setopt rcexpandparam
+setopt nocheckjobs
+setopt numericglobsort
+setopt nobeep
+setopt appendhistory
+setopt histignorealldups
+setopt autocd
+setopt inc_append_history
+setopt histignorespace
+
+zstyle ':completion"*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion"*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion"*' rehash true
+zstyle ':completion"*' accept-exact '*(N)'
+zstyle ':completion"*' use-cache on
+zstyle ':completion"*' cache-path ~/.zsh/cache
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=10000
+WORDCHARS=${WORDCHARS//\/[&.;]}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	ZSH_PLUGIN_DIR="/opt/homebrew/share"
+else
+	ZSH_PLUGIN_DIR="/usr/share/zsh/plugins"
+fi
+
+source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $ZSH_PLUGIN_DIR/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^H' backward-kill-word
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# My .dotfiles :)
+
+Usage:
+```sh
+# If you don't have git ssh: git clone --bare https://github.com/ArchGryphon9362/dotfiles $HOME/.dotfiles
+git clone --bare git@github.com:ArchGryphon9362/dotfiles $HOME/.dotfiles
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+```
+
+Now restart your terminal and enjoy (use kitty+zsh+ohmyzsh+nvim for the best experience)
