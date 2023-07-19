57978da236e5bc687022beb9b3d0bbb752de35c3
Lex Nastin <lexnastin@gmail.com>
2023-07-19 17:55:59 +0300

    make editor transparent

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -3,6 +3,16 @@ return {{
     name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme catppuccin]])
+        vim.cmd.colorscheme("catppuccin")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     end
 }}
+-- return {{
+--     "yashguptaz/calvera-dark.nvim",
+--     priority = 1000,
+--     config = function()
+--         vim.g.calvera_transparent_bg = true
+--
+--         require("calvera").set()
+--     end
+-- }}
