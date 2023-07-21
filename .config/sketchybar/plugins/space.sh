850f68c2087091a40355f710ac7e7bd45041260f
Lex Nastin <lexnastin@gmail.com>
2023-07-21 23:23:53 +0200

    hide unused spaces in sketchybar

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 COLOR=0x20000000
+PARSING_ID=0
+DRAWING="off"
 
 if [[ $SELECTED == "true" ]]; then
     COLOR=0x80000000
 fi
 
-sketchybar --set $NAME background.color=$COLOR
+if [[ $(yabai -m query --spaces --space $SID | jq -re ".windows != [] or .\"has-focus\"") == "true" ]]; then
+    DRAWING="on"
+fi
+
+sketchybar --set $NAME background.color=$COLOR drawing=$DRAWING
