fa584287d967f9ad6dd2a76ac3d30317e6558007
Lex Nastin <lexnastin@gmail.com>
2023-08-08 00:34:19 +0100

    changed yabai again and added home wifi profile

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -12,4 +12,14 @@ if [[ $IP != "" ]]; then
     IP=" - ${IP}"
 fi
 
+# wifi name is a very sensitive thing so i hid it :) (gl cracking,
+# it's >9 chars long and on my M1 Pro machine takes ~750ms to hash a
+# single password...)
+htpasswd -vb ~/.config/sketchybar/wifiname.txt "wifiname" "$INFO"
+if [[ $? == 0 ]]; then
+    networksetup -switchtolocation Home
+else
+    networksetup -switchtolocation Automatic
+fi
+
 sketchybar --set $NAME label="${WIFI}${IP}" icon="$ICON"
