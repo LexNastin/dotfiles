2daa49ac4e78cdaa0417ce53d3c5bed57cc4bf7c
Lex Nastin <lexnastin@gmail.com>
2023-11-07 23:06:06 +0000

    macos sonoma wifi name fix

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')"
+
 WIFI=${INFO:-"Not Connected"}
 ICON="󰤨"
 IP=$(ipconfig getifaddr en0)
