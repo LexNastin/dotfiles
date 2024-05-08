652a0de75b136b59deba2006a27d956b46f9dab2
Lex Nastin <lexnastin@gmail.com>
2024-05-08 22:39:34 +0100

    many linux fixes

--- .config/eww/scripts/hyprland.sh
+++ .config/eww/scripts/hyprland.sh
@@ -4,7 +4,7 @@ WORKSPACE=$(hyprctl activeworkspace -j | jq ".id")
 
 handle() {
     case "$1" in
-        workspace*)
+        workspace\>*)
             WORKSPACE=$(echo "$1" | cut -f3 -d">")
             ;;
     esac
@@ -13,4 +13,4 @@ handle() {
 }
 
 handle "workspace>>$WORKSPACE"
-socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+socat -U - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
--- .config/eww/scripts/wifi.sh
+++ .config/eww/scripts/wifi.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-WIFI_NAME=$(nmcli -g "GENERAL.CONNECTION" d show wlan0)
+WIFI_NAME=$(nmcli -g "GENERAL.CONNECTION" d show wlp1s0f0)
 if [ ! "$WIFI_NAME" ]; then
     echo '{"icon": "󰤭", "message": "Not Connected"}'
     exit
 fi
 
-IP_ADDRESS=$(nmcli -g "IP4.ADDRESS" d show wlan0 | cut -f1 -d"/")
+IP_ADDRESS=$(nmcli -g "IP4.ADDRESS" d show wlp1s0f0 | cut -f1 -d"/")
 
 echo "{\"icon\": \"󰤥\", \"message\": \"$WIFI_NAME - $IP_ADDRESS\"}"
