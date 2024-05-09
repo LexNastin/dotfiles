6b79a73142b0316c75117e07deeedae89c8ed2a0
Lex Nastin <lexnastin@gmail.com>
2024-05-10 01:10:46 +0200

    minor fix for wifi fix

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
+if [[ $INFO == "You are not associated with an AirPort network." ]]; then
+    INFO=""
+fi
 
 WIFI=${INFO:-"Not Connected"}
 ICON="󰤨"
