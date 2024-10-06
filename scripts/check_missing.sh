8040cb0fbe23d263296afa635bf7eadd303b589d
Lex Nastin <lexnastin@gmail.com>
2024-10-06 15:00:12 +0100

    fix missing duplicate

--- scripts/check_missing.sh
+++ scripts/check_missing.sh
@@ -11,9 +11,12 @@ missing=()
 [ ! $(command -v neofetch ) ] && missing+=("neofetch")
 [ ! $(command -v sudo) ] && missing+=("sudo")
 [ ! $(command -v node) ] && missing+=("node.js")
-[ ! $(command -v pyenv) ] && missing+=("pyenv")
-[ ! $(command -v pyenv) ] && missing+=("pyenv") && missing+=("pyenv-virtualenv")
-[ $(command -v pyenv) ] && [[ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]] && missing+=("pyenv-virtualenv")
+if [ $(command -v pyenv) ]; then
+    [[ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]] && missing+=("pyenv-virtualenv")
+else
+    missing+=("pyenv")
+    [[ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]] && missing+=("pyenv-virtualenv")
+fi
 
 # fonts
 [ $(command -v fc-list) ] && [ -z "$(fc-list | grep "FiraCode Nerd Font Mono")" ] && missing+=("FiraCode Nerd Font Mono")
